d3715efa8a49ef5332060f77b4c1824ff7a5a009
Hamsaleka <35956740+Hamsaleka@users.noreply.github.com>
2018-05-29 16:47:06 +0530

    s

--- src/main/java/com/kgisl/nsedata/controller/NseController.java
+++ src/main/java/com/kgisl/nsedata/controller/NseController.java
@@ -3,23 +3,25 @@ package com.kgisl.nsedata.controller;
 import java.util.*;
 import com.kgisl.nsedata.*;
 import com.kgisl.nsedata.model.NseData;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
 import io.swagger.models.Model;
 
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*")
 @RequestMapping("/")
 public class NseController {
 
@@ -30,23 +32,21 @@ public class NseController {
 		//RestTemplate restTemplate ;
 	//  RestTemplate <restTemplate> ;
 	
-		@RequestMapping(value="/{category}",method={RequestMethod.GET})
-		public @ResponseBody String showUserTable(@RequestParam("category") String cat){
-			System.out.println(cat);
-			RestTemplate restTemplate = new RestTemplate();
+	//	@RequestMapping(value="/category",method={RequestMethod.POST})
+		@RequestMapping(value = "/category/{cat}", method = RequestMethod.GET)
+		public  ResponseEntity<?>  showUserTable(@PathVariable String cat,UriComponentsBuilder builder){
+			System.out.println("???????????????"+cat);
+		RestTemplate restTemplate = new RestTemplate();
+			NseData ns=new NseData();
 		  String nseDatas = restTemplate.getForObject("https://www.nseindia.com/live_market/dynaContent/live_watch/stock_watch/"+cat+"StockWatch.json", String.class);  // or however I use restTemplates, havent done it yet so still fuzzy but shouldnt be too tricky.    
-		//   String data = String.valueOf(nseDatas);
-		//   System.out.println(data);
-		  return nseDatas;
-		 
-		//   model.addAttribute("nseDatas", nseDatas);gi
-		//   return "server"; // returns a jsp simply looping through the list and displaying.
+		  return new ResponseEntity<>(nseDatas,HttpStatus.OK);	 
 		}
 
 		@RequestMapping(value={"Data"},method={RequestMethod.GET})
 		public @ResponseBody String showUserTable1(){
 			System.out.println("---------------------------------");
 			return null;
+	
 		}
 }
 
